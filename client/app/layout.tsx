4f3d94daf754c2386e8f00f4966905120ce45bd6
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-03-18 11:49:17 -0400

    create Exaclidraw Wrapper

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <AuthProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           suppressHydrationWarning
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image';
-import logo from '@/public/logo.png';
-import { Inter } from 'next/font/google';
+'use client';
 
-const inter = Inter({
-  weight: ['400'],
-  subsets: ['latin'],
-});
+import dynamic from 'next/dynamic';
+
+// Since client components get prerenderd on server as well hence importing
+// the excalidraw stuff dynamically with ssr false
+
+const ExcalidrawWrapper = dynamic(
+  async () => (await import('../components/ExaclidrawWrapper')).default,
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
-    <div className="w-auto mx-auto h-screen flex flex-col justify-center items-center gap-10">
-      <div className="w-auto mx-auto flex justify-center items-center">
-        <p className={`${inter.className} antialiased text-5xl text-wrap`}>
-          Amiggle – Learn, Collaborate, Succeed! 🚀📚
-        </p>
-      </div>
-      <Image src={logo} alt="logo" width={500} height={500} />
+    <div className="w-full border overflow-auto">
+      <ExcalidrawWrapper />
     </div>
   );
 }
